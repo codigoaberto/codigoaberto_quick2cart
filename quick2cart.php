6215dc381910213eb1183c94e50ef9dc0274d4fb
Codigo Aberto <marcio.marques@codigoaberto.pt>
2015-04-06 23:36:01 +0100

    Update to new quick2cart version

--- quick2cart.php
+++ quick2cart.php
@@ -8,17 +8,26 @@
 * @license 			GNU General Public License version 2 or later; see _LICENSE.php
 **/
 
-defined( '_JEXEC' ) or die;
+defined('_JEXEC') or die('Restricted access');
+
+if (!defined('DS'))
+{
+	define('DS', '/');
+}
+
+
+
 jimport('joomla.plugin.plugin');
 $lang = JFactory::getLanguage();
 $lang->load('plg_content_content_quick2cart', JPATH_ADMINISTRATOR);
-$path = JPATH_SITE.DS.'components'.DS.'com_quick2cart'.DS.'helper.php';	
-if(!class_exists('comquick2cartHelper'))
-{
-  //require_once $path;
-   JLoader::register('comquick2cartHelper', $path );
-   JLoader::load('comquick2cartHelper');
-}
+$path = JPATH_SITE.DS.'components'.DS.'com_quick2cart'.DS.'helper.php';
+//if (! class_exists('comquick2cartHelper'))
+//{
+	// Require_once $path;
+	JLoader::register('comquick2cartHelper', $path);
+	JLoader::load('comquick2cartHelper');
+//}
+//jimport('joomla.plugin.plugin');
 // Plugin
 class plgCCK_FieldQuick2cart extends JCckPluginField
 {
@@ -85,18 +94,22 @@ class plgCCK_FieldQuick2cart extends JCckPluginField
 		$attr	=	'class="'.$class.'" size="'.$field->size.'"'.$maxlen . ( $field->attributes ? ' '.$field->attributes : '' );
 		if($field->storage_location) {
 		jimport('joomla.filesystem.file');
+	
 		if( !JFile::exists( JPATH_SITE.'/components/com_quick2cart/quick2cart.php') ){
 			return true;
 		}
 		$name_form = 'com_content.article';
 		if(isset($config['storages']['#__content']->title)) JRequest::setVar("qtc_article_name",$config['storages']['#__content']->title);
 		$doc = JFactory::getDocument();
-		$doc->addStyleSheet(JURI::base().'components'.DS.'com_quick2cart'.DS.'css'.DS.'quick2cart.css');
+		$doc->addStyleSheet(JUri::root(true) .'components/com_quick2cart/assets/css/quick2cart.css');
+	   $doc->addStyleDeclaration('.cck_label_quick2cart { display:none;}');
+		$doc->addScript(JURI::root(true).'/media/techjoomla_strapper/js/namespace.min.js');
+		$doc->addScript(JURI::root(true).'/components/com_quick2cart/assets/js/order.js');
       $doc->addScript(self::$path.'assets/quick2cart.js');
 
 		// Add the registration fields to the form.
 	jimport('joomla.form.helper');
-JFormHelper::loadFieldClass('list');
+       JFormHelper::loadFieldClass('list');
 		$lang = JFactory::getLanguage();
 		$lang->load('com_quick2cart', JPATH_ADMINISTRATOR);
 		$fieldName = $name;
@@ -106,18 +119,28 @@ JFormHelper::loadFieldClass('list');
 		//$pid = JRequest::getVar('id');
 		$jinput = JFactory::getApplication()->input;
 		$pid=$jinput->get('id');
-		if($pid) {
+	//	if($pid) {
 		// CHECK for view override	
-		$comquick2cartHelper = new comquick2cartHelper();
+		/*	$mainframe = JFactory::getApplication();
+				$lang = JFactory::getLanguage();
+				$lang->load('com_quick2cart');
+				$comquick2cartHelper = new comquick2cartHelper();
+			$path = $comquick2cartHelper->getBuynow($pid, "com_content", array());
+	
+	ob_start();
+		include($path);
+		$html = ob_get_contents();
+		ob_end_clean();	*/
+				$comquick2cartHelper = new comquick2cartHelper();
 	 	$path=$comquick2cartHelper->getViewpath('attributes','',"ADMIN","SITE");
 		ob_start();
 			include($path);
 		$html = ob_get_contents();
-		ob_end_clean();				
-		}
-		else{
-			$html = "<br/><p style='float: left;'>".JText::_('QTC_ADDATTRI_EMPTY_MSG_DESC')."</p>";
-		}
+		ob_end_clean();					
+		//}
+	//	else{
+	//		$html = "<br/><p style='float: left;'>".JText::_('QTC_ADDATTRI_EMPTY_MSG_DESC')."</p>";
+	//	}
 
 		$form =  '<div id="quick2cart_info">'.$html.'</div>';
 
