8fa373d15ec1edd2eaafa8b660887612a8aa435f
Codigo Aberto <marcio.marques@codigoaberto.pt>
2015-04-06 23:37:48 +0100

    Update to new quick2cart version

--- assets/quick2cart.js
+++ assets/quick2cart.js
@@ -3,4 +3,5 @@ jQuery(document).ready(function() {
 	jQuery('#cck_tabs1Tabs').append('<li><a href="#tab_quick2cart" data-toggle="tab">Quick2Cart</a></li>');
  	jQuery('#cck_tabs1Content').append('<div id="tab_quick2cart" class="tab-pane">'+jQuery('#quick2cart_info').html()+'</div>');
 	jQuery('#quick2cart_info').html('');
+	document.adminForm = document.seblod_form;
 });
